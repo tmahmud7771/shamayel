2be889aec5c0f3869e43a8328c7d145c5a5907a6
tausif mahmud <tmahmud44@gmail.com>
2023-01-26 06:30:59 +0600

    v1

---
+++ app.js
@@ -0,0 +1,92 @@
+/*************************************************************************************
+* WEB322 - 2231 Project
+* I declare that this assignment is my own work in accordance with the Seneca Academic
+* Policy. No part of this assignment has been copied manually or electronically from
+* any other source (including web sites) or distributed to other students.
+*
+* Student Name  : Shamayel Asim
+* Student ID    : 
+* Course/Section: WEB322 NCC
+*
+**************************************************************************************/
+
+// const path = require("path");
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+// Add your routes here
+// e.g. app.get() { ... }
+
+//routes
+
+// Routes for the home page
+app.get("/", (req, res) => {
+    // res.send("home page");
+    res.render("home");
+});
+
+// Routes for the rental page
+app.get("/rentals", (req, res) => {
+    // res.send("rentals page");
+    res.render("rentals");
+});
+
+// Routes for the singup page
+app.get("/sign-up", (req, res) => {
+    // res.send("sign-up page");
+    res.render("sign_up");
+});
+
+// Routes for the login page
+app.get("/log-in", (req, res) => {
+    // res.send("log-in page");
+    res.render("login");
+});
+
+
+
+
+// *** DO NOT MODIFY THE LINES BELOW ***
+
+// This use() will not allow requests to go beyond it
+// so we place it at the end of the file, after the other routes.
+// This function will catch all other requests that don't match
+// any other route handlers declared before it.
+// This means we can use it as a sort of 'catch all' when no route match is found.
+// We use this function to handle 404 requests to pages that are not found.
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+// This use() will add an error handler function to
+// catch all errors.
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send("Something broke!")
+});
+
+
+
+
+
+
+
+
+
+
+// Define a port to listen to requests on.
+const HTTP_PORT = process.env.PORT || 8080;
+
+// Call this function after the http server starts listening for requests.
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+  
+// Listen on port 8080. The default port for http is 80, https is 443. We use 8080 here
+// because sometimes port 80 is in use by other applications on the machine
+app.listen(HTTP_PORT, onHttpStart);
